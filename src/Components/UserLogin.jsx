8b15ab70dad36e324bb41aa190c5fa348b8cdbcf
Chauncey Darby <chaunceydrb@gmail.com>
2024-06-06 15:28:08 +0200

    updated

--- src/Components/UserLogin.jsx
+++ src/Components/UserLogin.jsx
@@ -1,11 +1,16 @@
 
-import { FaReact } from 'react-icons/fa'
+import { FaReact } from 'react-icons/fa6'
 
 const UserLogin = () => {
   return (
     <div>
         <div>
             <FaReact />
+            <h1>Drop Chat</h1>
+        </div>
+        <div>
+            <input type="text" placeholder='Enter a Unique Name' />
+            <button>Login</button>
         </div>
     </div>
   )
