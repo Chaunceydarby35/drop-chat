bccb341f53feb8d9870efc3cf8776ed1cc817efc
Chauncey Darby <chaunceydrb@gmail.com>
2024-07-09 20:34:51 +0200

    updated

---
+++ src/Components/ChatLists.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const ChatLists = () => {
+    function SenderChat () {
+        return (
+    
+        )
+    }
+  return (
+    <div>ChatLists</div>
+  )
+}
+
+export default ChatLists
